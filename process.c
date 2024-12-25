fe500061dc41b70226c17de7510be6992fdf8f5b
hunglqdz <101244308+hunglqdz@users.noreply.github.com>
2024-12-25 10:28:06 +0100

    Add files via upload

---
+++ process.c
@@ -0,0 +1,22 @@
+#include<sys/types.h>
+#include<stdio.h>
+#include<unistd.h>
+
+int main(){
+    pid_t pid;
+    pid = fork(); // fork a child process
+
+    if(pid<0){ // error occurred
+        fprintf(stderr,"Fork failed");
+        return 1;
+    }
+    else if(pid==0){ // child process
+        execlp("/bin/ls","ls",NULL);
+    }
+    else{ // parent process
+        wait(NULL); // wait for child to complete
+        printf("Child complete");
+    }
+
+    return 0;
+}
